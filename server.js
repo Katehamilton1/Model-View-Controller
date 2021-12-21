8d247a7ea74317128867266c60ab7c91ca43a529
Kate Hamilton <kate.hamilton828@gmail.com>
2021-12-21 13:32:14 +0300

    update

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const exphbs = require('express-handlebars');
 
 const session = require('express-session');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -39,5 +40,5 @@ app.use(routes)
 
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+  app.listen(PORT, () => console.log('Now listening'));
+});
